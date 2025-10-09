71c55d94864ae86a319055cd0ea984ae52259a57
RedstoneMango <RedStoneMango@outlook.com>
2025-10-09 14:12:50 +0200

    implemented more robust handling of errors during I/O config load

--- src/main/java/io/github/redstonemango/mangoplayer/logic/config/SongConfigWrapper.java
+++ src/main/java/io/github/redstonemango/mangoplayer/logic/config/SongConfigWrapper.java
@@ -5,7 +5,10 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonSyntaxException;
 import com.google.gson.annotations.Expose;
 import io.github.redstonemango.mangoplayer.graphic.MangoPlayer;
+import io.github.redstonemango.mangoplayer.logic.GlobalMenuBarActions;
 import io.github.redstonemango.mangoplayer.logic.Song;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
@@ -18,6 +21,7 @@ public class SongConfigWrapper {
     private static SongConfigWrapper INSTANCE = null;
 
     public static final String configFilePath = MangoPlayer.APP_FOLDER_PATH + "/songs.json";
+    public static boolean loadError = false;
 
     @Expose public LinkedHashMap<String, Song> songs;
 
@@ -31,6 +35,12 @@ public class SongConfigWrapper {
     }
 
     public static synchronized void save() {
+        if (loadError) {
+            System.out.println("Not saving song config due to an error during its I/O load!"); // Do not save (i.e. possibly overwrite) the config file if loading failed
+            return;
+        }
+        System.out.println("Saving song config...");
+
         File file = new File(configFilePath);
         try {
             if (!file.exists()) {
@@ -52,8 +62,21 @@ public class SongConfigWrapper {
             return new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create().fromJson(scanner.next(), SongConfigWrapper.class);
         } catch (FileNotFoundException e) {
             return null;
-        } catch (JsonSyntaxException e) {
-            System.err.println("Invalid JSON syntax in song config file: " + e);
+        } catch (Exception e) {
+            loadError = true;
+            System.err.println("Error loading song config:");
+            e.printStackTrace(System.err);
+
+            ButtonType errorButton = new ButtonType("Submit issue");
+            Alert alert = new Alert(Alert.AlertType.ERROR, "", errorButton, ButtonType.CLOSE);
+            alert.setTitle("FknPlayer | Config I/O Error");
+            alert.setHeaderText("An error occurred while loading the configuration file '" + new File(configFilePath).getName() + "'");
+            alert.setContentText("This might be due to a malformed JSON text.\nPlease submit an issue if you are unable to fix this");
+            alert.showAndWait();
+            if (alert.getResult() == errorButton) {
+                GlobalMenuBarActions.onIssuesMenu();
+            }
+            MangoPlayer.primaryStage.close();
             return null;
         }
     }
